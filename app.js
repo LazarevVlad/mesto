e0cfc323b22e676d7803bf006752bfd52f555c03
Vladislav Lazarev <lazarevvladv@yandex.ru>
2020-06-15 00:35:52 +0700

    added userRouter

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const path = require("path");
+const userRouter = require("./routes/users");
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(express.static(__dirname, "/public"));
-app.listen(PORT);
+app.use("/users", userRouter);
+app.use(express.static(path.join(__dirname, "/public")));
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
