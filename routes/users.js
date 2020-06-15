2ededfe78d410419fc0e47761a0b070603187bec
Vladislav Lazarev <lazarevvladv@yandex.ru>
2020-06-15 15:38:33 +0700

    fixed users router

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,29 @@
 const router = require("express").Router();
 const path = require("path");
 const userPath = path.join(__dirname, "../data/users.json");
-
 const users = require(userPath);
 
-router.get("/", (req, res) => {
+function getUser(val) {
+  return users.find((user) => {
+    return user._id === val;
+  });
+}
+
+router.get("/", (req, res, next) => {
   res.send(users);
   next();
 });
 
 router.get("/:_id", (req, res, next) => {
-  if (!users[req.params._id]) {
-    res.send(`Такого пользователя не существует`);
+  const user = getUser(req.params._id);
+  // console.log(req.params._id);
+  // console.log(user);
+  if (user) {
+    res.send(user);
     return;
   }
-  res.send(users[req.params._id]);
+  res.status(404);
+  res.send({ message: "Нет пользователя с таким id" });
   next();
 });
 module.exports = router;
