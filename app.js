969b15734c7195bbcb550a2fa1d0c6c5b5b2630f
Vladislav Lazarev <lazarevvladv@yandex.ru>
2020-06-15 16:19:16 +0700

    fixed app

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const path = require("path");
-const userRouter = require("./routes/users");
+const users = require("./routes/users");
+const cards = require("./routes/cards");
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use("/users", userRouter);
+app.use("/users", users);
+app.use("/cards", cards);
 app.use(express.static(path.join(__dirname, "/public")));
+app.use((err, req, res, next) => {
+  res.status(404);
+  res.json({ message: "Запрашиваемый ресурс не найден" });
+});
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
