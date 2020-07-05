f717e235352596dfcd2fd65091f401fd541b2053
Vladislav Lazarev <lazarevvladv@yandex.ru>
2020-07-06 01:13:41 +0700

    fixed user's model

--- models/card.js
+++ models/card.js
@@ -2,13 +2,21 @@ const mongoose = require("mongoose");
 
 const cardSchema = new mongoose.Schema({
   name: {
-    type: String,
+    type: mongoose.Schema.Types.String,
     required: true,
     minlength: 2,
     maxlength: 30,
   },
   link: {
-    type: String,
+    type: mongoose.Schema.Types.String,
+    validate: {
+      validator(v) {
+        return /http(s)?:\/{2}(w{3}.)?(((([\w\d]+(-\w+)+)|(\w{2,}))(.[a-z]{2,6})+)|(([1-9]|([1-9]\d)|(1\d\d)|(2[1-4]\d)|(25[0-5]))(.([0-9]|([1-9]\d)|(1\d\d)|(2[0-4]\d)|(25[0-5]))){2}.([1-9]|([1-9]\d)|(1\d\d)|(2[1-4]\d)|(25[0-5]))))((:(([1-9]\d)|([1-9]\d\d)|([1-9]\d\d\d)|([1-9])))?)((\/[a-z]{2,})*(#)?)/.test(
+          v
+        );
+      },
+      message: "Введите ссылку",
+    },
     required: true,
   },
   owner: {
@@ -17,12 +25,12 @@ const cardSchema = new mongoose.Schema({
     ref: "user",
   },
   likes: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: [mongoose.Schema.Types.ObjectId],
     default: [],
-    ref: "user._id",
+    ref: "user",
   },
   createdAt: {
-    type: Date,
+    type: mongoose.Schema.Types.Date,
     default: Date.now,
   },
 });
