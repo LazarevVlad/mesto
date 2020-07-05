1dbc55560812340ff82ef6abb6a0fa603d999f12
Vladislav Lazarev <lazarevvladv@yandex.ru>
2020-07-06 01:13:59 +0700

    fixed user's model

--- models/user.js
+++ models/user.js
@@ -2,19 +2,19 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: {
-    type: String,
+    type: mongoose.Schema.Types.String,
     required: true,
     minlength: 2,
     maxlength: 30,
   },
   about: {
-    type: String,
+    type: mongoose.Schema.Types.String,
     required: true,
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
-    type: String,
+    type: mongoose.Schema.Types.String,
     required: true,
   },
 });
