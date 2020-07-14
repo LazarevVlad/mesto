fb89f6d8e61b90df440511dd90d8e1adef081bcf
Vladislav Lazarev <lazarevvladv@yandex.ru>
2020-07-14 23:31:12 +0700

    created middleware

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+const handleAuthError = (res) =>
+  res.status(401).send({ message: "Необходима авторизация" });
+
+const extractBearerToken = (header) => header.replace("Bearer", "");
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.starrWith("Bearer")) {
+    return handleAuthError(res);
+  }
+
+  const token = extractBearerToken(authorization);
+  let payload;
+
+  try {
+    payload = jwt.verify(token, "secret");
+  } catch (err) {
+    return handleAuthError(res);
+  }
+  req.user = payload;
+  next();
+};
