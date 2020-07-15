ec42617d7628bd6ff64a75d8ba95c5586dd55a09
Vladislav Lazarev <lazarevvladv@yandex.ru>
2020-07-15 18:10:08 +0700

    fixed statics

--- models/user.js
+++ models/user.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
-userSchema.static.findUserByCredentials = function (email, password) {
+userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email })
     .select("+password")
     .then((user) => {
